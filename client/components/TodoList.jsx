b2cfb97d9a95ee711ba6270c7bcf8646883b7cb8
Sahinur Islam <infosahinur@gamil.com>
2023-08-21 07:33:41 +0600

    [UPDATE] vite

--- client/components/TodoList.jsx
+++ client/components/TodoList.jsx
@@ -54,7 +54,7 @@ const TodoList = () => {
 
     return (
         <div >
-            <h1>What's the Plan for Today?</h1>
+            <h1 className='title-from'>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo}/>
             <Todo
                 allTodo={allTodo}
---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import TodoList from '../components/TodoList'
+
+function App() {
+
+  return (
+    <>
+     <TodoList />
+    </>
+  )
+}
+
+export default App
